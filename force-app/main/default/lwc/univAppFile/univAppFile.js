b95ef80657864a93fbc59d6e2ef818ed35c66da3
Jake Karl <jake.karl@radianhub.com>
2023-12-11 18:24:44 +0000

    Email templates, flow bug, metadata labels, remove file functionality

--- force-app/main/default/lwc/univAppFile/univAppFile.js
+++ force-app/main/default/lwc/univAppFile/univAppFile.js
@@ -4,11 +4,13 @@ export default class UnivAppFile extends LightningElement {
 	@api field;
 
 	file;
-	fileName;
 
+	// # HANDLERS
+
+	// *
 	handleChangeFile(event) {
 		const file = event.target.files[0];
-		this.fileName = this.field.label + ' - ' + file.name;
+		this.file = file;
 
 		const fileSelectedEvent = new CustomEvent('fileselected', {
 			detail: {
@@ -18,5 +20,35 @@ export default class UnivAppFile extends LightningElement {
 			},
 		});
 		this.dispatchEvent(fileSelectedEvent);
+	}
+
+	// *
+	handleClickRemove() {
+		this.file = undefined;
+
+		const fileRemovedEvent = new CustomEvent('fileremoved', {
+			detail: {
+				fieldApiName: this.field.api,
+			},
+		});
+		this.dispatchEvent(fileRemovedEvent);
+	}
+
+	// # GETTERS
+
+	// *
+	get hasFile() {
+		if (this.file !== undefined) {
+			return true;
+		}
+		return false;
+	}
+
+	// *
+	get fileName() {
+		if (this.file !== undefined) {
+			return this.field.label + ' - ' + this.file.name;
+		}
+		return null;
 	}
 }
